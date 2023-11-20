36dd4d7c211e6b70dfcd6197ea3f5d0e3ee02ca9
Your Name <you@example.com>
2023-11-20 22:05:17 +0400

    Todo App

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,26 @@
+import React, { useState } from 'react';
+
 function TodoList({ todos, setTodos }) {
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState('');
+
   function deleteTodo(index) {
     const updatedTodos = [...todos];
     updatedTodos.splice(index, 1);
     setTodos(updatedTodos);
+  }
+
+  function editBtn(index) {
+    setEditIndex(index);
+    setEditText(todos[index]);
+  }
+
+  function saveEdit(index) {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = editText;
+    setTodos(updatedTodos);
+    setEditIndex(null);
+    setEditText('');
   }
 
   return (
@@ -11,10 +29,30 @@ function TodoList({ todos, setTodos }) {
         <ul>
           {todos.map((todo, index) => (
             <li key={index}>
-              {index + 1}. {todo}
-              <button className="deleteBtn" onClick={() => deleteTodo(index)}>
-                Delete
-              </button>
+              {index + 1}. {editIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                  />
+                  <button className="saveBtn" onClick={() => saveEdit(index)}>
+                    Save
+                  </button>
+                </>
+              ) : (
+                <>
+                  {todo}
+                  <div>
+                    <button className="deleteBtn" onClick={() => deleteTodo(index)}>
+                      Delete
+                    </button>
+                    <button className="editBtn" onClick={() => editBtn(index)}>
+                      Edit
+                    </button>
+                  </div>
+                </>
+              )}
             </li>
           ))}
         </ul>
@@ -22,4 +60,5 @@ function TodoList({ todos, setTodos }) {
     </>
   );
 }
+
 export default TodoList;
